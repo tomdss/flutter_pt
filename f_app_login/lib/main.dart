5784067cbebfba6e4484664e169d8cf2da8ed9be
liemtt <liemtt@rikkeisoft.com>
2021-07-28 18:48:24 +0700

    add ui login white bg

--- f_app_login/lib/main.dart
+++ f_app_login/lib/main.dart
@@ -18,61 +18,88 @@ class MyAppState extends State<MyApp> {
       home: Scaffold(
         body: Container(
           constraints: BoxConstraints.expand(),
-          padding: EdgeInsets.fromLTRB(20, 100, 20, 30),
-          color: Colors.black,
+          padding: EdgeInsets.fromLTRB(20, 0, 20, 150),
+          color: Colors.white,
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text('Hello :)',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold)),
-              Text('Welcome Back',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold)),
-              SizedBox(height: 25),
-              Text('USERNAME',
-                  style: TextStyle(color: Colors.grey, fontSize: 15)),
-              TextFormField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), hintText: 'Enter username'),
+              Container(
+                  width: 70,
+                  height: 70,
+                  padding: EdgeInsets.all(15),
+                  margin: EdgeInsets.fromLTRB(0, 0, 0, 30),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Color(0xffe5e5e5)),
+                  child: FlutterLogo()),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                child: Text('Hello\nWelcome Back',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold)),
               ),
-              SizedBox(height: 20),
-              Text('PASSWORD',
-                  style: TextStyle(color: Colors.grey, fontSize: 15)),
-              TextFormField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), hintText: 'Enter password'),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
+                child: TextField(
+                  style: TextStyle(color: Colors.black, fontSize: 18),
+                  decoration: InputDecoration(
+                      labelText: 'USERNAME',
+                      labelStyle: TextStyle(color: Colors.grey, fontSize: 16),
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter username'),
+                ),
               ),
-              SizedBox(height: 20),
               Stack(
-                alignment: AlignmentDirectional.center,
+                alignment: AlignmentDirectional.centerEnd,
                 children: [
-                  Container(
-                    constraints: BoxConstraints.expand(height: 50),
-                    decoration: new BoxDecoration(
-                        color: Colors.blueAccent,
-                        shape: BoxShape.rectangle,
-                        borderRadius: BorderRadius.all(Radius.circular(6))),
+                  TextField(
+                    style: TextStyle(color: Colors.black, fontSize: 18),
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        labelText: 'PASSWORD',
+                        labelStyle: TextStyle(color: Colors.grey, fontSize: 16),
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter password'),
                   ),
-                  Text('SIGN IN',
-                      style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold))
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Text('SHOW',
+                        style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold)),
+                  )
                 ],
               ),
               SizedBox(height: 20),
-              Row(
-                children: [
-                  Text('NEW USER? ',
-                      style: TextStyle(color: Colors.grey, fontSize: 10)),
-                  Text(' SIGN UP',
-                      style: TextStyle(color: Colors.blueAccent, fontSize: 10)),
-                  SizedBox(width: 100),
-                  Text('FORGOT PASSWORD',
-                      style: TextStyle(color: Colors.blueAccent, fontSize: 10)),
-                ],
+              SizedBox(
+                width: double.infinity,
+                height: 56,
+                child: RaisedButton(
+                  color: Colors.blue,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                  onPressed: onSignInClick,
+                  child: Text('SIGN IN',style: TextStyle(color: Colors.white,fontSize: 16,fontWeight: FontWeight.bold),),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 50,
+                width: double.infinity,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('NEW USER? ',
+                        style: TextStyle(color: Colors.grey, fontSize: 14,fontWeight: FontWeight.bold)),
+                    Text(' SIGN UP',
+                        style: TextStyle(color: Colors.blue, fontSize: 14,fontWeight: FontWeight.bold)),
+                    SizedBox(width: 100),
+                    Text('FORGOT PASSWORD',
+                        style: TextStyle(color: Colors.blue, fontSize: 14,fontWeight: FontWeight.bold)),
+                  ],
+                ),
               )
             ],
           ),
@@ -80,4 +107,6 @@ class MyAppState extends State<MyApp> {
       ),
     );
   }
+
+  void onSignInClick() {}
 }
